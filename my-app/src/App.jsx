173c43d54715c05424ab8d66652f4ea46fe64499
Evan Bray <bray.ev@northeastern.edu>
2026-02-24 16:23:38 -0500

    commented out app.jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -11,8 +11,9 @@ import MarketplacePage from './pages/MarketplacePage'
 function App() {
   return (
     <>
+    {/* defining the routes that map URL paths to corresponding page components */}
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<LoginPage />} /> {/* root path, first thing user will see */}
         <Route path="/marketplace" element={<MarketplacePage />} />
         <Route path="/listing-details" element={<ListingDetailPage />} />
         <Route path="/add-listing" element={<AddListingPage />} />
