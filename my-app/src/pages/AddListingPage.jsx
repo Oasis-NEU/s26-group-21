9819c9c0fa4ca02050444f1ff74eaa7c89c944ec
Evan Bray <bray.ev@northeastern.edu>
2026-02-24 18:13:34 -0500

    created skeletal listing detail page with placeholder information

--- my-app/src/pages/AddListingPage.jsx
+++ my-app/src/pages/AddListingPage.jsx
@@ -1,7 +1,7 @@
 // creating React component (reusable piece of UI)
 // export makes it available to be imported by other files
 export default function AddListingPage() {
-    // <div> is a placeholder for text, will be replaced later
+    // <div> groups content together
     // what will be displayed on screen
     return <div>Add Listing Page</div>
 }
